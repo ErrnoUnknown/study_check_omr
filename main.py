a3ddc8f0a2d3633ebbcc035d555572b340350b4e
stingray0225 <114566779+ErrnoUnknown@users.noreply.github.com>
2024-06-05 15:51:30 +0900

    Main.py update

---
+++ main.py
@@ -0,0 +1,14 @@
+# Import
+import csv
+
+# CSV 파일 읽기
+with open('student.csv', 'r', encoding='utf-8') as file:
+    reader = csv.reader(file)
+
+    for row in reader:
+        print(f'이름: {row[0]}')
+        print(f'학년: {row[1]}')
+        print(f'반: {row[2]}')
+        print(f'번호: {row[3]}')
+        print('=' * 10)
+        print()
