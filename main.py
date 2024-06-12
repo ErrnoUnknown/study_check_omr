2416cfa98d137a5b5688a523524eebb7b98b451e
stingray0225 <114566779+ErrnoUnknown@users.noreply.github.com>
2024-06-12 15:08:57 +0900

    IDK

--- main.py
+++ main.py
@@ -32,16 +32,19 @@ def generate_study_check_doc():
     # 학생 데이터 가져오기
     students = mdl_sql.get_all_students()
 
-    # 학생 정보 추가
-    mdl_image.create_white_image('temp/white.png', (250, 2500))
+    # 결과 이미지 틀 생성
+    mdl_image.create_white_image('temp/result.png', (2000, 2000))
 
+    # 결과 이미지 편집
     for index, student in enumerate(students):
+        # 학생 ID QR 코드 생성 및 저장
         img = qrcode.make(str(student['id']), box_size=10, border=0)
 
-        # 학생 ID QR코드 생성 및 저장
         img.save('temp/qr.png')
         mdl_image.resize_img('temp/qr.png', 'temp/qr.png', (100, 100))
 
-        mdl_image.put_image_over('temp/white.png', 'temp/qr.png', 'temp/white.png', (50, index * 200))
+        # aaa
+        mdl_image.put_image_over('temp/result.png', 'temp/qr.png', 'temp/result.png', (50, (index + 1) * 200))
 
+        mdl_image.put_text_over('temp/result.png', 'temp/result.png', )
 generate_study_check_doc()
--- module/image.py
+++ module/image.py
@@ -1,27 +1,57 @@
 # 라이브러리 불러오기
 import cv2
+from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 
 # 이미지 크기 재조정 함수 정의
 def resize_img(input_path, output_path, size):
+    # 이미지 열기
     img = cv2.imread(input_path)
+
+    # 이미지 처리
     img = cv2.resize(img, size)
+
+    # 이미지 저장
     cv2.imwrite(output_path, img)
 
 # 이미지 생성 함수 정의
 def create_white_image(output_path, size):
+    # 이미지 생성
     img = np.ones((size[1], size[0], 3), dtype=np.uint8) * 255
+
+    # 이미지 저장
     cv2.imwrite(output_path, img)
 
 # 이미지 오버레이 함수 정의
 def put_image_over(bg_input_path, overlay_input_path, output_path, position):
+    # 이미지 열기
     bg_img = cv2.imread(bg_input_path)
     overlay_img = cv2.imread(overlay_input_path)
 
+    # 이미지 처리
     overlay_height, overlay_width = overlay_img.shape[:2]
 
     x, y = position
 
     bg_img[y:y+overlay_height, x:x+overlay_width] = overlay_img
 
+    # 이미지 저장
     cv2.imwrite(output_path, bg_img)
+
+# 텍스트 오버레이 함수 정의
+def put_text_over(input_path, output_path, position, text, text_color, size, text_ttf_path):
+    # 이미지 열기
+    img = cv2.imread(input_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = Image.fromarray(img)
+
+    # 폰트 열기
+    font = ImageFont.truetype(text_ttf_path, size)
+
+    # 이미지 처리
+    img = ImageDraw.Draw(img)
+    img.text(position, text, font=font, fill=text_color + (0))
+
+    # 이미지 저장
+    img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+    cv2.imwrite(output_path, img)
