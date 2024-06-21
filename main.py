01489139f437ca2fed79271a4962010f8a1bd121
stingray0225 <114566779+ErrnoUnknown@users.noreply.github.com>
2024-06-21 22:14:13 +0900

    Result Generation

--- main.py
+++ main.py
@@ -25,17 +25,47 @@ def add_students():
                 mdl_sql.insert_student(name=row[0], grade=row[1], class_number=row[2], number=row[3])
 
 # 출석 체크 문서 생성 함수 정의
-def generate_study_check_doc():
+def generate_study_check_doc(study_name, year, month, date):
     # 학생 데이터 가져오기
     students = mdl_sql.get_all_students()
 
-    # 결과 이미지 편집
+    # 베이스 이미지 편집
         # 결과 이미지 틀 생성
-    mdl_image.create_white_image('temp/result.png', (2000, 2000))
+    mdl_image.create_white_image('temp/base.png', (1320, 1850))
 
         # 좌상단 인디케이터 추가
-    mdl_image.put_image_over('temp/result.png', 'template/top_left_indicator.png', 'temp/result.png', (0, 0))
+    mdl_image.put_image_over('temp/base.png', 'template/top_left_indicator.png', 'temp/base.png', (0, 0))
 
+        # 우하단 인디케이터 추가
+    mdl_image.put_image_over('temp/base.png', 'template/bottom_right_indicator.png', 'temp/base.png', (1270, 1800))
+
+    # 자습 정보 QR 코드 생성 및 저장
+    img = qrcode.make(f'{study_name}-{year}-{month}-{date}', box_size=10, border=0)
+
+    img.save('temp/qr.png')
+    mdl_image.resize_img('temp/qr.png', 'temp/qr.png', (100, 100))
+
+    mdl_image.put_image_over('temp/base.png', 'temp/qr.png', 'temp/base.png', (50, 50))
+
+    # 자습 이름 추가
+    mdl_image.put_text_over(input_path='temp/base.png',
+                                output_path='temp/base.png',
+                                position=(175, 50),
+                                text=study_name,
+                                text_color=(0, 0, 0, 0),
+                                size=65,
+                                text_ttf_path='font/pretendard_extra_bold.ttf')
+    
+    # 자습 날짜 추가
+    mdl_image.put_text_over(input_path='temp/base.png',
+                                output_path='temp/base.png',
+                                position=(175, 120),
+                                text=f'{year}년 {month}월 {date}일',
+                                text_color=(0, 0, 0, 0),
+                                size=30,
+                                text_ttf_path='font/pretendard_medium.ttf')
+
+    # 페이지 생성
     for index, student in enumerate(students):
         # 학생 ID QR 코드 생성 및 저장
         img = qrcode.make(str(student['id']), box_size=10, border=0)
@@ -43,19 +73,21 @@ def generate_study_check_doc():
         img.save('temp/qr.png')
         mdl_image.resize_img('temp/qr.png', 'temp/qr.png', (100, 100))
 
-        mdl_image.put_image_over('temp/result.png', 'temp/qr.png', 'temp/result.png', (50, (index * 150) + 50))
-        
+        mdl_image.put_image_over('temp/base.png', 'temp/qr.png', 'temp/result.png', (50, (index * 150) + 300))
+
+        # 이름 추가
         mdl_image.put_text_over(input_path='temp/result.png',
                                 output_path='temp/result.png',
-                                position=(170, (index * 150) + 50),
+                                position=(170, (index * 150) + 300),
                                 text=student['name'],
                                 text_color=(0, 0, 0, 0),
                                 size=30,
                                 text_ttf_path='font/pretendard_extra_bold.ttf')
 
+        # 학적 추가
         mdl_image.put_text_over(input_path='temp/result.png',
                                 output_path='temp/result.png',
-                                position=(170, (index * 150) + 90),
+                                position=(170, (index * 150) + 340),
                                 text=f'{student["grade"]}학년 {student["class_number"]}반 {student["number"]}번',
                                 text_color=(0, 0, 0, 0),
                                 size=30,
@@ -65,4 +97,4 @@ def generate_study_check_doc():
 mdl_sql.create_db()
 
 add_students()
-generate_study_check_doc()
+generate_study_check_doc('심야 자율학습', 2008, 2, 25)
